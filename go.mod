66cda33fcedf71ac296613f1535fdcc60c39327a
nvm <Never M>
2020-01-28 11:52:34 +0200

    Implement basic http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module nvm.ga/tododo
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.5.0
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+const TODO_NUM = 20
+
+func initMockTodos() []*Todo {
+	todos := make([]*Todo, 0)
+	for i := 0; i < TODO_NUM; i++ {
+		now := time.Now()
+		todos = append(todos, &Todo{
+			ID:          uint(i + 1),
+			Title:       fmt.Sprintf("Title %d", i),
+			Description: fmt.Sprintf("Description %d", i),
+			CreatedAt:   now,
+			UpdatedAt:   now,
+			ExpiresAt:   now.AddDate(0, 0, i),
+		})
+	}
+	return todos
+}
+
+func MockTodoMiddleware(c *gin.Context) {
+	todos := initMockTodos()
+	c.Set("total_todos", len(todos))
+}
 
 func main() {
-	fmt.Println("cyka")
+	r := gin.Default()
+	r.Use(MockTodoMiddleware)
+	r.GET("/ping", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"message":     "pong",
+			"total_todos": c.GetInt("total_todos"),
+		})
+	})
+	r.GET("/pong", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":     "ping",
+			"total_todos": c.GetInt("total_todos"),
+		})
+	})
+	r.Run()
 }
